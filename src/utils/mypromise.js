59682043b537e195f4f9760ca5adb4d93ea59b14
lakshmanaj <72659550+lakshmanaj@users.noreply.github.com>
2022-07-26 09:05:57 +0530

    common promises

---
+++ src/utils/mypromise.js
@@ -0,0 +1,20 @@
+import Branch from "../models/branchModel.js";
+
+export const isProcessed_Branch = (data) => (
+    new Promise((resolve, reject) => {
+        Branch.findOne({ "_id": data.branchid })
+            .then(function (result) {
+                return result;
+            })
+            .then(function (result) {
+
+                if (result.is_validated == false) {
+                    reject({ message: "Branch is not validated" });
+                }
+                if (result.is_active == false) {
+                    reject({ message: "Branch is not active" });
+                }
+                resolve(result);
+            });
+    })
+);
